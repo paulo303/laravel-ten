0983cf940a7a92a330aaa56d9a4741f375fb15aa
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2023-03-09 02:24:46 -0300

    wip

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Maatwebsite\Excel\Facades\Excel;
 class UserController extends Controller
 {
     public function exportUsers()
-    {
+{
         return Excel::download(new UserExport(), 'users.xlsx');
     }
 }
