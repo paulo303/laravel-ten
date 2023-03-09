050c3e73a8f701bc092528b20e6bbdfe10150bed
Paulo Cavalcanti <paulocavalcanti303@gmail.com>
2023-03-09 02:25:13 -0300

    wip

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -17,16 +17,16 @@ class UserController extends Controller
      */
     public function index(): Response
     {
-        // Log::alert("Atenção: obrigado pela atenção", [
-        //     'class' => self::class,
-        //     'name'  => 'Paulo', 'email' => 'maie@maie.com'
-        // ]);
-        // Log::critical("critical");
-        // Log::debug("debug");
-        // Log::emergency("emergency");
-        // Log::error("error");
-        // Log::info("info");
-        // Log::notice("notice");
+        Log::alert("Atenção: obrigado pela atenção", [
+            'class' => self::class,
+            'name'  => 'Paulo', 'email' => 'maie@maie.com'
+        ]);
+        Log::critical("critical");
+        Log::debug("debug");
+        Log::emergency("emergency");
+        Log::error("error");
+        Log::info("info");
+        Log::notice("notice");
 
         return response(User::all());
     }
